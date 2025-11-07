1f4d87c6d73bf4882c55961e938b0d99b6f4513d
NguyenThiTuyetTram <tramntt03@gmail.com>
2025-11-08 01:44:08 +0700

    Thêm chức năng thống kê tỉ lệ hỏng hóc theo chi nhánh

--- warrantyclaim-api/src/main/java/com/warrantyclaim/warrantyclaim_api/repository/WarrantyClaimRepository.java
+++ warrantyclaim-api/src/main/java/com/warrantyclaim/warrantyclaim_api/repository/WarrantyClaimRepository.java
@@ -2,7 +2,10 @@ package com.warrantyclaim.warrantyclaim_api.repository;
 
 
 import com.warrantyclaim.warrantyclaim_api.entity.WarrantyClaim;
+import com.warrantyclaim.warrantyclaim_api.enums.WarrantyClaimStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +14,13 @@ import java.util.List;
 public interface WarrantyClaimRepository extends JpaRepository<WarrantyClaim, String> {
 
     public List<WarrantyClaim> findByCustomerName(String customerName);
+
+    @Query("SELECT wc.officeBranch, COUNT(wc) " +
+            "FROM WarrantyClaim wc " +
+            "WHERE wc.status IN (:statuses) AND wc.officeBranch IS NOT NULL " +
+            "GROUP BY wc.officeBranch")
+    List<Object[]> getClaimCountByOfficeBranch(@Param("statuses") List<WarrantyClaimStatus> statuses);
+    long countByStatusIn(List<WarrantyClaimStatus> statuses);
 
 
 }
